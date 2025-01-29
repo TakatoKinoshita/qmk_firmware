8b57965269e0bfe7a08b219fa08406a679796d07
TakatoKinoshita <takato@ieee.org>
2025-01-30 02:54:27 +0900

    modify meta layer

--- keyboards/helix/rev3_4rows/keymaps/TakatoKinoshita/keymap.c
+++ keyboards/helix/rev3_4rows/keymaps/TakatoKinoshita/keymap.c
@@ -50,10 +50,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_META] = LAYOUT(
-    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LGUI,                   KC_ESC,  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    
-    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_MINS,                   KC_EQL,  KC_H,    KC_J,    KC_K,    KC_L,    KC_SLSH, 
-    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_SCLN,                   KC_QUOT, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_BSLS, 
-    TG_NUM,  KC_LALT, KC_TAB,  UP_SPC,  SC_LSPO, KC_ENT,  TGL_IME, XXXXXXX, KC_BSPC, SC_RCPC, UP_LBRC, KC_RBRC, KC_GRV,  TG_FUNC 
+    _______, _______, _______, _______, _______, KC_LGUI,                   _______, _______, _______, _______, _______, _______, 
+    _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, 
+    _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, 
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
   ),
 
   [_NUM] = LAYOUT(
