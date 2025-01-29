a929eebc017f59b92cc70645dff80646e94eb82c
TakatoKinoshita <takato@ieee.org>
2025-01-30 07:41:53 +0900

    swap shift and control

--- keyboards/helix/rev3_4rows/keymaps/TakatoKinoshita/keymap.c
+++ keyboards/helix/rev3_4rows/keymaps/TakatoKinoshita/keymap.c
@@ -34,10 +34,10 @@ enum layer_names {
 
 #define TGL_IME C(KC_SPC)
 
-#define SFT_ALT KC_LSFT | KC_LALT
+#define SFT_ALT S(KC_LALT)
 #define SFT_TAB S(KC_TAB)
 #define SFT_ENT S(KC_ENT)
-#define SFT_CTL KC_LSFT | KC_LCTL
+#define SFT_CTL S(KC_LCTL)
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    XXXXXXX,                   KC_ESC,  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    
     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_MINS,                   KC_EQL,  KC_H,    KC_J,    KC_K,    KC_L,    KC_SLSH, 
     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_SCLN,                   KC_QUOT, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_BSLS, 
-    TG_NUM,  KC_LALT, KC_TAB,  UP_SPC,  SC_LSPO, KC_ENT,  TGL_IME, XXXXXXX, KC_BSPC, SC_RCPC, UP_LBRC, KC_RBRC, KC_GRV,  TG_FUNC 
+    TG_NUM,  KC_LALT, KC_TAB,  UP_SPC,  SC_LCPO, KC_ENT,  TGL_IME, XXXXXXX, KC_BSPC, SC_RSPC, UP_LBRC, KC_RBRC, KC_GRV,  TG_FUNC 
   ),
 
   [_META] = LAYOUT(
@@ -74,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, _______,                   _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, 
     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_UNDS,                   KC_PLUS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_QUES, 
     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_COLN,                   KC_DQUO, _______, _______, KC_LABK, KC_RABK, KC_PIPE, 
-    _______, SFT_ALT, SFT_TAB, KC_SPC,  KC_LSFT, SFT_ENT, _______, _______, KC_DEL,  SFT_CTL, KC_LCBR, KC_RCBR, KC_TILD, _______
+    _______, SFT_ALT, SFT_TAB, KC_SPC,  SFT_CTL, SFT_ENT, _______, _______, KC_DEL,  KC_LSFT, KC_LCBR, KC_RCBR, KC_TILD, _______
   )
 
 };
@@ -165,13 +165,14 @@ const rgblight_segment_t PROGMEM rgb_raise[] = RGBLIGHT_LAYER_SEGMENTS(
     {11, 2,  HSV_CORAL},
     {13, 5,  HSV_BLUE},
     {19, 2,  HSV_CORAL},
-    {23, 1,  HSV_CORAL},
+    {22, 2,  HSV_CORAL},
     {26, 5,  HSV_MAGENTA},
     {31, 1,  HSV_CORAL},
     {32, 4,  HSV_ORANGE},
     {36, 2,  HSV_CORAL},
     {40, 3,  HSV_CORAL},
-    {44, 5,  HSV_CORAL}
+    {44, 3,  HSV_CORAL},
+    {48, 1,  HSV_CORAL}
 );
 
 const rgblight_segment_t* const PROGMEM rgb_layers[] = RGBLIGHT_LAYERS_LIST(
